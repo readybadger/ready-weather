c96d89f92f9270db7e79ed326c140e806d85325e
Ross <snapper-appeals4c@icloud.com>
2026-07-01 16:41:59 +0200

    Add query config for Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import prettierConfig from 'eslint-config-prettier'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -16,6 +17,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       prettierConfig,
+      pluginQuery.configs['flat/recommended'],
     ],
     languageOptions: {
       globals: globals.browser,
