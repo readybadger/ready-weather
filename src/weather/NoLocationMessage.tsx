cdc2d382b3636df778c0d6b3b15135592989d184
Ross <snapper-appeals4c@icloud.com>
2026-07-02 21:40:30 +0200

    Update NoLocationMessage

--- src/weather/NoLocationMessage.tsx
+++ src/weather/NoLocationMessage.tsx
@@ -1,10 +1,10 @@
 export const NoLocationMessage = () => {
   return (
-    <div className="flex flex-col justify-center bg-mist-50 p-8 rounded-md w-md border-b-emerald-200 border-b">
+    <div className="flex flex-col justify-center bg-white p-8 rounded-md w-md border-b-emerald-200 border-b">
       <h3 className="text-xl mb-2">Welcome to Ready Weather!</h3>
-      <p className="text-stone-600">
-        To get started, select a location by using the search bar at the top of
-        the page.
+      <p className="text-stone-600 text-sm">
+        Let's get you ready for what's coming - select a location by using the
+        search bar above to get started.
       </p>
     </div>
   )
