0b2bcbc86241564321e361ae1ed554940d845820
Ross <snapper-appeals4c@icloud.com>
2026-07-02 09:55:10 +0200

    Use selected location from settings context

--- src/weather/WeatherPage.tsx
+++ src/weather/WeatherPage.tsx
@@ -1,16 +1,25 @@
-import { useState } from 'react'
 import { CenterColumn } from '../layout/CenterColumn'
 import { Weather } from './Weather'
 import type { Location } from '../location/types'
 import { NoLocationMessage } from './NoLocationMessage'
 import { LocationSearch } from '../location/LocationSearch'
+import useSettings from '../settings/use-settings'
 
 export const WeatherPage = () => {
-  const [location, setLocation] = useState<Location>()
+  const {
+    settings: { location },
+    updateSettings,
+  } = useSettings()
+
+  const onSelectLocation = (newLocation: Location) => {
+    updateSettings({
+      location: newLocation,
+    })
+  }
 
   return (
     <CenterColumn className="py-6">
-      <LocationSearch onSelectLocation={setLocation} />
+      <LocationSearch onSelectLocation={onSelectLocation} />
       {location ? <Weather coords={location.coords} /> : <NoLocationMessage />}
     </CenterColumn>
   )
